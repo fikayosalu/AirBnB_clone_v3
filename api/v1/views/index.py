331671b4642421b54ea7efda835d7c009f70b92e
fikayosalu <salufikayo@gmail.com>
2025-02-17 16:51:32 +0100

    Set up proper Documentations

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+""" Set up response for status code """
 from api.v1.views import app_views
 from flask import jsonify
 
 
 @app_views.route("/status")
 def response_status():
+    """ Return status of the API """
     return jsonify({"status": "OK"})
