08d4fbc2b1221c594a0dad182568ea1a4000fe70
Oluwaseun Otetumo <annyauthe@yahoo.com>
2025-02-19 04:53:45 +0100

    Try parse

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -9,36 +9,40 @@ from models.city import City
 from models.state import State
 
 
-@app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<state_id>/cities', methods=['GET'],
+                 strict_slashes=False)
 def get_all_cities(state_id):
     """Returns all cities by state id"""
     state = storage.get(State, state_id)
-    if not state:
+    if state is None:
         abort(404)
-    return jsonify([city.to_dict() for city in state.cities])
+    return jsonify([city.to_json() for city in state.cities])
 
-@app_views.route('/cities/<city_id>', methods['GET'], strict_slashes=False)
+@app_views.route('/cities/<city_id>', methods['GET'],
+                 strict_slashes=False)
 def get_city(city_id):
     """Retrieves a city object by city id"""
-    city = storage.get(City, city_id)
-    if not city:
+    city = storage.get("City", str(city_id))
+    if city is None:
         abort(404)
-    return jsonify(city.to_dict())
+    return jsonify(city.to_json())
 
-@app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/cities/<city_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_city(city_id):
     """Deletes a city object by id"""
-    city = storage.get(City, city_id)
-    if not city:
+    city = storage.get("City", str(city_id))
+    if city is None:
         abort(404)
     storage.delete(city)
     storage.save()
     return jsonify({}), 200
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+@app_views.route('/states/<state_id>/cities', methods=['POST'],
+                 strict_slashes=False)
 def create_city(state_id):
     """Creates a new city object by state ID"""
-    state = storage.get(State, state_id)
+    state = storage.get("State", str(state_id))
     if not state:
         abort(404)
     try:
@@ -51,7 +55,7 @@ def create_city(state_id):
     storage.new(new_city)
     storage.save()
 
-    return jsonify(new_city.to_dict()), 201
+    return jsonify(new_city.to_json()), 201
 
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
 def update_city(city_id):
